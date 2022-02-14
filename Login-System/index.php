8730414bf638bf35f15945f5b01337eb69caa60f
Maikls <maikls.zalkalns@gmail.com>
2022-02-14 13:01:13 +0200

    Create index.php

---
+++ Login-System/index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div>
+        <div class="index-login-signup">
+            <h4>SIGN UP</h4>
+            <form action="includes/signup.inc.php" method="post">
+                <input type="text" name="uid" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <input type="password" name="pwd-repeat" placeholder="Repeat Password">
+                <input type="text" name="email" placeholder="E-mail">
+                <br>
+                <button type="submit" name="submit">SIGN UP</button>
+            </form>
+        </div>
+        <div class="index-login-login">
+            <h4>LOGIN</h4>
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="uid" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <br>
+                <button type="submit" name="submit">LOGIN</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
